af35391a9ffd3a39a25b3ab04b09a6e81d7cc7c6
Bertrand Pelletier <pelletierber@gmail.com>
2022-11-28 23:20:49 +0100

    first files

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import json
+import io
+import plotly.express as px
+import plotly.graph_objects as go
+from streamlit import session_state
+# from utils import *
+import pickle
